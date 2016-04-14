afa78cd989d2354f9c25665605d20d5448c9c3d0
Iñaki Cabo Alonso <icaboalo@gmail.com>
2016-04-13 21:16:50 -0500

    token model

--- app/src/main/java/com/icaboalo/bicycle/io/model/BicycleApiModel.java
+++ app/src/main/java/com/icaboalo/bicycle/io/model/BicycleApiModel.java
@@ -7,9 +7,69 @@ import com.google.gson.annotations.SerializedName;
  */
 public class BicycleApiModel {
 
+    public BicycleApiModel(String bicycleBrand, String bicycleModel, String bicycleTrack, String bicycleColor, String bicycleYear, String bicycleLatitude, String bicycleLongitude) {
+        mBicycleBrand = bicycleBrand;
+        mBicycleModel = bicycleModel;
+        mBicycleTrack = bicycleTrack;
+        mBicycleColor = bicycleColor;
+        mBicycleYear = bicycleYear;
+        mBicycleLatitude = bicycleLatitude;
+        mBicycleLongitude = bicycleLongitude;
+    }
+
     @SerializedName("id")
     int mBicycleId;
 
     @SerializedName("brand")
     String mBicycleBrand;
+
+    @SerializedName("model")
+    String mBicycleModel;
+
+    @SerializedName("track")
+    String mBicycleTrack;
+
+    @SerializedName("color")
+    String mBicycleColor;
+
+    @SerializedName("year")
+    String mBicycleYear;
+
+    @SerializedName("latitude")
+    String mBicycleLatitude;
+
+    @SerializedName("longitude")
+    String mBicycleLongitude;
+
+    public int getBicycleId() {
+        return mBicycleId;
+    }
+
+    public String getBicycleBrand() {
+        return mBicycleBrand;
+    }
+
+    public String getBicycleModel() {
+        return mBicycleModel;
+    }
+
+    public String getBicycleTrack() {
+        return mBicycleTrack;
+    }
+
+    public String getBicycleColor() {
+        return mBicycleColor;
+    }
+
+    public String getBicycleYear() {
+        return mBicycleYear;
+    }
+
+    public String getBicycleLatitude() {
+        return mBicycleLatitude;
+    }
+
+    public String getBicycleLongitude() {
+        return mBicycleLongitude;
+    }
 }
